33ac2a99728a1d6463442e3e8c543497cec8a6af
clark <inkservice2@naver.com>
2020-06-23 14:47:09 +0900

    third commit

--- firstPage/views.py
+++ firstPage/views.py
@@ -8,7 +8,16 @@ reloadedModel = joblib.load('./models/RFModelforMPG.pkl')
 
 
 def index(request):
-    context = {'a': 'Hello'}
+    temp = {}
+    temp['cylinders'] = 8
+    temp['displacement'] = 307
+    temp['horsepower'] = 130
+    temp['weight'] = 3504
+    temp['acceleration'] = 12
+    temp['model_year'] = 70
+    temp['origin'] = 1
+    context = {'temp': temp}
+
     return render(request, 'index.html', context)
     # return HttpResponse({'a':1})
 
@@ -33,6 +42,7 @@ def predictMPG(request):
     testData = pd.DataFrame({'x': temp2}).transpose()
     scoreval = reloadedModel.predict(testData)[0]
 
-    context = {'scoreval': scoreval}
+    context = {'scoreval': scoreval, 'temp': temp}
+    # 기본값을 입력후 결과와 함께 입력한 값이 보여지도록 하기 위해 'temp':temp 를 context에 넣어준다.
 
     return render(request, 'index.html', context)
